1fbe715db54ee382e8520a5588df24744ac41271
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-04 17:53:48 -0600

    Task 6

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                for (int x = i + 1; x <= 9; x++)
+                {
+                    if (i != x)
+                    {
+                        Console.Write($"{i}{x}");
+                        if (i == 8 && x == 9)
+                        {
+                            Console.WriteLine("");
+                        }
+                        else
+                        {
+                            Console.Write(", ");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
