96b672b6e717654a5ec7d1498122d70809d271f5
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-03 21:11:28 -0600

    Task 11

--- csharp-hello_world/11-concat_edges/11-concat_edges.cs
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -5,7 +5,7 @@ class Program
 	static void Main(string[] args)
 	{
 		string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers.";
-		str = str.Replace("C# (pronounced \"See Sharp\") is a simple, modern, ","").Replace(", and type-safe", "").Replace(" language. C# has its roots","").Replace(" the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers."," C#");
+		str = str.Substring(49, 15) + str.Substring(79, 12) + str.Substring(118, 3) + str.Substring(101, 3);
 		Console.WriteLine(str);
         }
 }
