36b5ad672bc8e14d6cd08da8357842401b94bccc
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-04 17:02:36 -0600

    Task 5

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int index = 0; index <= 99; index++)
+            {
+                if (index == 99)
+                {
+                    Console.Write("99");
+                    break;
+                }
+                Console.Write($"{index:D2}, ");
+            }
+        }
+    }
+}
