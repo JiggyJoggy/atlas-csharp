df50c0bd57b1c767de64599069711cfc926047eb
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-04 16:49:27 -0600

    Task 4

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int index = 0; index <= 98; index++)
+            {
+                Console.Write($"{index} = 0x{index:x}\n");
+            }
+        }
+    }
+}
