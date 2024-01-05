6a46fbe0e6f17e3198524bf911fd7acf9ca9c2c0
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-05 12:56:35 -0600

    test

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,30 +1,27 @@
 ﻿using System;
 
-namespace _0_print_array
+class Array
 {
-    class Array
+    public static int[] CreatePrint(int size)
     {
-        public static int[] CreatePrint(int size)
-        {
-            int[] array = new int[size];
+        int[] array = new int[size];
 
-            if (size == 0)
-            {
-                Console.WriteLine();
-            }
-            else if (size < 0)
-            {
-                Console.WriteLine("Size cannot be negative");
-                return null;
-            }
-            else
-            {
-                for (int index = 0; index < size; index++)
-                {
-                    array[index] = index;
-                }
-            }
-            return array;
+        if (size == 0)
+        {
+            Console.WriteLine();
         }
+        else if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        else
+        {
+            for (int index = 0; index < size; index++)
+            {
+                array[index] = index;
+            }
+        }
+        return array;
     }
 }
