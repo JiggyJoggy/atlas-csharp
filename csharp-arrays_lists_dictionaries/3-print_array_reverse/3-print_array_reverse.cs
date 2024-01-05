fbaa147d866453c5c63f1c60d0cd44caa12c010e
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-05 14:48:55 -0600

    task 3

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length < 0)
+        {
+            Console.WriteLine();
+            return;
+        }
+        for (int index = array.Length - 1; index > array.Length; index--)
+        {
+            if (index == 0)
+            {
+                Console.Write($"{array[index]}");
+            }
+            else
+            {
+                Console.Write($"{array[index]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
