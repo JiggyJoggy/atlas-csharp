5eec09e7838ad7202c32ee38b67ac4182d6c27ff
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-04 19:00:47 -0600

    Task 7

---
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _7_islower
+{
+    class Character
+    {
+        public static bool IsLower(char c)
+        {
+            if (c >= 97 && c <= 122)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
