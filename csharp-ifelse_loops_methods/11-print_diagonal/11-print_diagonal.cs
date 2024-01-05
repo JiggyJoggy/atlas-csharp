f60f3bad772a6aff76abfdce29a85a8e540719c7
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-04 19:42:03 -0600

    Task 11

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _11_print_diagonal
+{
+    class Line
+    {
+        public static void PrintDiagonal(int length)
+        {
+            if (length <= 0)
+            {
+                Console.WriteLine("");
+            }
+            else
+            {
+                for (int index = 0; index <= length; index++)
+                {
+                    for (int space = 1; space < index; space++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine("\\");
+                }
+            }
+        }
+    }
+}
