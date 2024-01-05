5c8bbc526c921a8d2bd4f9c68298ef505db524c2
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-04 19:49:47 -0600

    Task 12

---
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int index = 1; index <= 100; index++)
+            {
+                if (index % 3 == 0 && index % 5 == 0)
+                {
+                    Console.Write("FizzBuzz");
+                }
+                else if (index % 3 == 0)
+                {
+                    Console.Write("Fizz");
+                }
+                else if (index % 5 == 0)
+                {
+                    Console.Write("Buzz");
+                }
+                else
+                {
+                    Console.Write($"{index}");
+                }
+                if (index == 100)
+                {
+                    Console.WriteLine("");
+                    break;
+                }
+                Console.Write(" ");
+            }
+        }
+    }
+}
