732470a1f293cbec618085d366b940bb29e97188
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-05 12:25:05 -0600

    Task 0

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -2,11 +2,29 @@
 
 namespace _0_print_array
 {
-    class Program
+    class Array
     {
-        static void Main(string[] args)
+        public static int[] CreatePrint(int size)
         {
-            Console.WriteLine("Hello World!");
+            if (size == 0)
+            {
+                Console.Write("");
+            }
+            else if (size < 0)
+            {
+                Console.Write("Size cannot be negative");
+                return null;
+            }
+            else
+            {
+                int[] array = new int[size];
+
+                for (int index = 0; index < size; index++)
+                {
+                    array[index] = index;
+                }
+                return array;
+            }
         }
     }
 }
