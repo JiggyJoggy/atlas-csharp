bb7fa197b71885be8610be51e8cef18eb552036e
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-04 19:28:09 -0600

    Task 10

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _10_print_line
+{
+    class Line
+    {
+        public static void PrintLine(int length)
+        {
+            if (length <= 0)
+            {
+                Console.WriteLine("");
+            }
+            else
+            {
+                for (int index = 0; index <= length; index++)
+                {
+                    Console.Write("_");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
