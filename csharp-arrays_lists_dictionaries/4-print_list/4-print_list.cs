5395ac6fc49da5b693bc226c446eed79ccd796ae
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-05 15:45:23 -0600

    task 4

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        List<int> list = new List<int>();
+
+        for (int index = 0; index < size; index++)
+        {
+            list.Add(index);
+        }
+        for (int x = 0; x < size; x++)
+        {
+            Console.Write(list[x]);
+            if (x == size - 1)
+            {
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.Write(" ");
+            }
+        }
+        if (size == 0)
+        {
+            Console.WriteLine();
+        }
+        return list;
+    }
+}
