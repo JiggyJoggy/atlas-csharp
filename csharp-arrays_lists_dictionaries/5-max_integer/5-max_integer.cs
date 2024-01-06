886ae66abb09e5b08364c3668c916f7b710eb593
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-05 18:05:20 -0600

    task 5

---
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+        else
+        {
+            int maxNum = myList[0];
+
+            foreach (int number in myList)
+            {
+                if (number > maxNum)
+                {
+                    maxNum = number;
+                }
+            }
+            return maxNum;
+        }
+    }
+}
