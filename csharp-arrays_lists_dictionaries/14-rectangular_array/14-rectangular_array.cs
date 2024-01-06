fe7446a1cfab1c99c8ef76e83dc9c0452eb36bf6
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-06 00:15:56 -0600

    task 14

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] rectangle = new int[5, 5];
+        rectangle[2, 2] = 1;
+
+        for (int col = 0; col < 5; col++)
+        {
+            for (int row = 0; row < 5; row++)
+            {
+                Console.Write($"{rectangle[col, row]}");
+                if (row < 4)
+                {
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
