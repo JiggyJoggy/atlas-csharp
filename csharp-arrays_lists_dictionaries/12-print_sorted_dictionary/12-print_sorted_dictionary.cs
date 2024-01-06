40beffef3122d96d6b56ea42b7def1782e35fc28
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-05 23:41:50 -0600

    task 12

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        ICollection<string> keys = myDict.Keys;
+        List<string> sortKeys = new List<string>(keys);
+        sortKeys.Sort();
+        foreach (var key in sortKeys)
+        {
+            Console.WriteLine($"{key}: {myDict[key]}");
+        }
+    }
+}
