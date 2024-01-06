c81b365182e72e050212559116b7cddb0fabfd50
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-06 00:06:07 -0600

    task 13

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Count <= 0)
+        {
+            return "None";
+        }
+        int currScore = -1;
+        string highscore = "";
+
+        foreach (var score in myList)
+        {
+            if (score.Value > currScore)
+            {
+                currScore = score.Value;
+                highscore = score.Key;
+            }
+        }
+        return highscore;
+    }
+}
