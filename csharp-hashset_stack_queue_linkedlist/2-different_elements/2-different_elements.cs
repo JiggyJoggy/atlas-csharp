03e3b75484469ace66cd70c9dcf30a6c6424e698
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-07 16:38:54 -0600

    task 2

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        SortedSet<int> sortDiff = new SortedSet<int>();
+        foreach (int number in list1)
+        {
+            if (!list2.Contains(number))
+            {
+                sortDiff.Add(number);
+            }
+        }
+        foreach (int number in list2)
+        {
+            if (!list1.Contains(number))
+            {
+                sortDiff.Add(number);
+            }
+        }
+        List<int> diffList = new List<int>(sortDiff);
+        return diffList;
+    }
+}
