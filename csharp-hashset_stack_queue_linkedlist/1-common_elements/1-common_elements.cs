2af05d94097d04514fc3bee6fbc03721d80f8101
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-07 15:36:21 -0600

    task 1

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> commonList = new List<int>();
+        foreach (int number in list1)
+        {
+            foreach (int number_2 in list2)
+            {
+                if (number == number_2)
+                {
+                    commonList.Add(number);
+                }
+            }
+        }
+        return commonList;
+    }
+}
