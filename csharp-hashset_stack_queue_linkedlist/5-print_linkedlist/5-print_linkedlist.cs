6b9da54e829930cfde8ceda4495cc9181d722d81
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-07 22:52:50 -0600

    task 5

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> newList = new LinkedList<int>();
+
+        if (size < 0)
+        {
+            return newList;
+        }
+        for (int index = 0; index < size; index++)
+        {
+            newList.AddLast(index);
+        }
+        foreach (int element in newList)
+        {
+            Console.WriteLine(element);
+        }
+        return newList;
+    }
+}
