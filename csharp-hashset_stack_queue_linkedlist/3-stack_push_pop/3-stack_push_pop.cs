65fad3fdaf0bb6481a90eb2ee94323102ee5a7d9
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-07 18:03:45 -0600

    task 3

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        // Number of items in aStack
+        Console.WriteLine($"Number of items: {aStack.Count}");
+
+        // Checking what item is at the top of aStack
+        var firstItem = aStack.Peek();
+        if (aStack.Count == 0)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            Console.WriteLine($"Top item: {firstItem}");
+        }
+
+        // Searching to see if it's in aStack
+        if (aStack.Contains(search))
+        {
+            Console.WriteLine($"Stack contains {search}: True");
+            while (aStack.Contains(search))
+            {
+                aStack.Pop();
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Stack contains {search}: False");
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
