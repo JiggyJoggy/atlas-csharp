5a6efc5cb1ba7a3b8f937e3031d6b8b5aed191d4
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-08 02:33:39 -0600

    task 8

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int valueIndex = -1;
+        int currIndex = -1;
+        foreach (var element in myLList)
+        {
+            currIndex++;
+            if (element == value)
+            {
+                valueIndex = currIndex;
+                return valueIndex;
+            }
+        }
+        return valueIndex;
+    }
+}
