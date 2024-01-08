adb578b3bb7cc81c6b7bfc33de8bfe84ac6f0ac1
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-08 15:23:32 -0600

    task 2 fix?

--- csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -5,36 +5,22 @@ class List
 {
     public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
     {
-        int sum = 0;
-        bool checkedAlready = false;
         List<int> newList = new List<int>();
         try
         {
-            for (int i = 0; i < listLength; i++)
+            for (int index = 0; index < listLength; index++)
             {
-                try
-                {
-                    if (list2[i] == 0 || list1[i] == 0)
-                    {
-                        newList.Add(0);
-                        throw new DivideByZeroException();
-                    }
-                    sum = list1[i] / list2[i];
-                    newList.Add(sum);
-                }
-                catch
-                {
-                    if (!checkedAlready)
-                    {
-                        Console.WriteLine("Cannot divide by zero");
-                        checkedAlready = true;
-                    }
-                }
+                newList.Add(list1[index]/list2[index]);
             }
         }
-        catch (IndexOutOfRangeException)
+        catch (DivideByZeroException)
         {
-            Console.WriteLine("Out of Range");
+            Console.WriteLine("Cannot divide by zero");
+            newList.Add(0);
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Out of range");
         }
         return newList;
     }
