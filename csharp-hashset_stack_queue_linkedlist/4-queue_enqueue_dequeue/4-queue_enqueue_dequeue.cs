788add06c8be117bd9e273edce0651a39cd18905
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-07 21:36:01 -0600

    task 4

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        // How many items are in Queue
+        Console.WriteLine($"Number of items: {aQueue.Count}");
+
+        if (aQueue.Count == 0)
+        {
+            Console.WriteLine($"Queue is empty");
+        }
+        else
+        {
+            Console.WriteLine($"First item: {aQueue.Peek()}");
+        }
+
+        aQueue.Enqueue(newItem);
+
+        if (aQueue.Contains(search))
+        {
+            Console.WriteLine($"Queue contains \"{search}\": True");
+            while (aQueue.Contains(search))
+            {
+                aQueue.Dequeue();
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Queue contains \"{search}\": False");
+        }
+        return aQueue;
+    }
+}
