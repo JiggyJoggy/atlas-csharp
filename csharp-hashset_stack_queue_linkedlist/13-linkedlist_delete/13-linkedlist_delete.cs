a255d13838ebea160dad34ddeb08332fc514ec26
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-08 05:09:43 -0600

    task 13

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int tempIndex = 0;
+
+        foreach (int element in myLList)
+        {
+            if (tempIndex == index)
+            {
+                myLList.Remove(element);
+                break;
+            }
+            tempIndex++;
+        }
+    }
+}
