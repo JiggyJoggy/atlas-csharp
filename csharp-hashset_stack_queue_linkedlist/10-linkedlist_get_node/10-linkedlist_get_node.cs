2deefa7ef98bc69a314b44ddc03811bcf44a8b75
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-08 03:31:24 -0600

    task 10

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int valueIndex = 0;
+        int currIndex = 0;
+        
+        foreach (var element in myLList)
+        {
+            currIndex++;
+            if (currIndex == n + 1)
+            {
+                valueIndex = currIndex;
+                return element;
+            }
+        }
+        return 0;
+    }
+}
