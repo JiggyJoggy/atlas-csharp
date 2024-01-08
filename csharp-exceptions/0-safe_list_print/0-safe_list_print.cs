d98fa20ba309495d5ec17a7c000f88a28f1ac094
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-08 05:35:56 -0600

    task 0

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int tempIndex = 0;
+        try
+        {
+            for (int index = 0; index < n; index++)
+            {
+                Console.WriteLine($"{myList[index]}");
+                tempIndex++;
+            }
+        }
+        catch
+        {
+
+        }
+        return tempIndex;
+    }
+}
