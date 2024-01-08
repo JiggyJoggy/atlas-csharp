1ff9fd1d28e2b9384392c5aa834744eae03e46a1
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-08 03:12:39 -0600

    task 9

---
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        int saveData = 0;
+        if (myLList.Count > 0)
+        {
+            saveData = myLList.First.Value;
+            myLList.Remove(myLList.First);
+            return saveData;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
