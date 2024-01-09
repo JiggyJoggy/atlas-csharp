434c1b8c85989aaa06f1a16fab1ef34a7e146faa
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-09 17:49:51 -0600

    task 1

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Public class Zombie with health
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Zombie's HP
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Zombie object with setting health to 0
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
