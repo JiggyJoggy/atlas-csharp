6cc4e41673bcf892c2d7cf97727fb642861e5ae4
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-09 18:33:59 -0600

    task 3

---
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Public class Zombie with health
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Zombie's private int health
+        /// </summary>
+        private int health;
+
+        /// <summary>
+        /// Zombie object with setting health to 0
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        /// <summary>
+        /// Sets health to the value given
+        /// Throws an ArgumentException if health is less than 0
+        /// </summary>
+        /// <param name="value"></param>
+        /// <exception cref="ArgumentException"></exception>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+
+            this.health = value;
+        }
+
+        /// <summary>
+        /// Return Zombie's health
+        /// </summary>
+        /// <returns></returns>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
