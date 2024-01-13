2cd464b7095ef7271a996a3236dd52aac256e977
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-13 16:23:26 -0600

    Task 2

--- csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -12,6 +12,9 @@ namespace MyMath.Tests
 
         [Test]
         [TestCase(new int[] {1, 2, 3, 4, 5}, 5)]
+        [TestCase(new int[] {7, 4, 3, 2, 1}, 7)]
+        [TestCase(new int[] {1, 2, 9, 4, 5}, 9)]
+        [TestCase(new int[] {-5, 7, 6, 10, 2}, 10)]
         [TestCase(new int[] {}, 0)]
         public void Test_List_Output(int[] num, int expectedSum)
         {
