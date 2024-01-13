85d0496bfd7f7283e904fd7f1a2e20d6365f87c1
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-13 15:30:01 -0600

    Task 1

--- csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -10,32 +10,30 @@ namespace MyMath.Tests
         {
             testMatrix = new int[2, 2];
             testMatrix[0, 0] = 2;
-            testMatrix[0, 1] = 4
-            testMatrix[1, 0] = 6
+            testMatrix[0, 1] = 4;
+            testMatrix[1, 0] = 6;
             testMatrix[1, 1] = 8;
         }
 
-        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
         public void Test_Matrix_Output(int value)
         {
             int[,] createdMatrix = MyMath.Matrix.Divide(testMatrix, value);
             
             Assert.AreEqual(2 / value, createdMatrix[0, 0]);
-            Assert.Pass()
         }
 
         [Test]
         public void Test_Null_Return()
         {
             Assert.IsNull(MyMath.Matrix.Divide(null, 2));
-            Assert.Pass()
         }
 
         [Test]
         public void Test_Divide_By_Zero()
         {
             Assert.IsNull(MyMath.Matrix.Divide(testMatrix, 0));
-            Assert.Pass()
         }
     }
 }
