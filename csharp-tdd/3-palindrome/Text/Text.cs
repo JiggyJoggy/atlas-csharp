150fa7a6118004adbb61fb2495af21dd96f70c8d
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-13 17:47:40 -0600

    test

---
+++ csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Text
+{
+    public class Str
+    {
+        public static bool IsPalindrome(string s)
+        {
+            if (s == "")
+            {
+                return true;
+            }
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                if (s[i] != s[s.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
