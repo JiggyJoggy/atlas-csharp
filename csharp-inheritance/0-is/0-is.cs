b6086b3dfd352968a016fab6db401262f70056f6
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-14 01:23:08 -0600

    XML

--- csharp-inheritance/0-is/0-is.cs
+++ csharp-inheritance/0-is/0-is.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 
 class Obj
 {
+    /// <summary>
+    /// Checking to obj's type
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
     public static bool IsOfTypeInt(object obj)
     {
         return (obj is int);
