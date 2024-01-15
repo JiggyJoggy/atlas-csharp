8cb6dee75b725cfa22e6f091b962c9321bce10c8
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-15 01:49:44 -0600

    sleep time, saving work

---
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+abstract class Base
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{name} is a {GetType().Name}";
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+
+    void Collect();
+}
+
+class Door : Base, IInteractive
+{
+    public void Interact()
+    {
+        
+    }
+}
