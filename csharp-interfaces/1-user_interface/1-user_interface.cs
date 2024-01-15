bdcd79462479b1d1ec5b2904545a3facd34bf159
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-15 01:41:43 -0600

    task 1

---
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+abstract class Base
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{name} is a {GetType().Name}";
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+
+    void Collect();
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public void Interact()
+    {
+
+    }
+
+    public int durability { get; set; }
+
+    public void Break()
+    {
+
+    }
+
+    public bool isCollected { get; set; }
+
+    public void Collect()
+    {
+        
+    }
+}
