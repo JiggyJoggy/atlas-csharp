0874077981c806ef1f69d75d96daa01317aeb4d7
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-19 19:55:53 -0600

    task 6

---
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length && vector2.Length == 2)
+        {
+            double sumOne = vector1[1] + vector2[1];
+            double sumTwo = vector1[2] + vector2[2];
+
+            double[] sum = [sumOne, sumTwo];
+
+            return Math.Round(sum, 2);
+        }
+        else if (vector1.Length && vector2.Length == 3)
+        {
+            double sumOne = vector1[1] + vector2[1];
+            double sumTwo = vector1[2] + vector2[2];
+            double sumThree = vector1[3] + vector2[3];
+
+            double[] sum = [sumOne, sumTwo, sumThree];
+
+            return Math.Round(sum, 2);
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
