f2672821097cdea6a60b88908527d5ca92d555e0
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-19 21:03:20 -0600

    task 9

---
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        if (vector.Length == 2)
+        {
+            double sumOne = vector[0] * scalar;
+            double sumTwo = vector[1] * scalar;
+
+            double[] sum = {sumOne, sumTwo};
+
+            return sum;
+        }
+        else if (vector.Length == 3)
+        {
+            double sumOne = vector[0] * scalar;
+            double sumTwo = vector[1] * scalar;
+            double sumThree = vector[2] * scalar;
+
+            double[] sum = {sumOne, sumTwo, sumThree};
+
+            return sum;
+        }
+        else
+        {
+            double[] emptyVector = {-1};
+            return emptyVector;
+        }
+    }
+}
