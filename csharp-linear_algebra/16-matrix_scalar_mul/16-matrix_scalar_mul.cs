bc36f7dc6d2c3023afa4af694eae74eae3450e5d
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-20 16:07:19 -0600

    task 16

---
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        if (rows > 1 && cols > 1)
+        {
+            double[,] sum = new double[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    sum[i, j] = matrix[i, j] * scalar;
+                }
+            }
+            return sum;
+        }
+        else
+        {
+            double[,] badMatrix = {{-1}};
+            return badMatrix;
+        }
+    }
+}
