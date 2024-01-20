8be05860077e7166e8a0fe890306f428489ebb3d
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-19 19:04:11 -0600

    task 3 fix?

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,23 +1,20 @@
 ﻿using System;
 
-namespace _3_magnitude
+class VectorMath
 {
-    class VectorMath
+    public static double Magnitude(double[] vector)
     {
-        public static double Magnitude(double[] vector)
+        if (vector.Length == 2)
         {
-            if (vector.Length == 2)
-            {
-                return Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1]);
-            }
-            else if (vector.Length == 3)
-            {
-                return Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2]);
-            }
-            else
-            {
-                return -1;
-            }
+            return Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1]);
+        }
+        else if (vector.Length == 3)
+        {
+            return Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2]);
+        }
+        else
+        {
+            return -1;
         }
     }
 }
