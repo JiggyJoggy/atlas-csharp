b4a2539c1d37e8c7425e37dfcc7c0d6f2b61891f
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-20 16:44:12 -0600

    task 18

---
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
+        int rows = matrix1.GetLength(0);
+        int cols = matrix1.GetLength(1);
+
+        int rows1 = matrix2.GetLength(0);
+        int cols1 = matrix2.GetLength(1);
+
+        if (cols != rows1)
+        {
+            double[,] allSum = new double[rows, cols1];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols2; j++)
+                {
+                    double sum = 0.0;
+
+                    for (int k = 0; k < cols; k++)
+                    {
+                        sum += matrix1[i, k] * matrix2[k, j];
+                    }
+                }
+            }
+            return allSum[i, j] = sum;
+        }
+        else
+        {
+            double[,] badMatrix = {{-1}};
+            return badMatrix;
+        }
+    }
+}
