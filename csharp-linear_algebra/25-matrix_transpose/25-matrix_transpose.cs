8c179987ae6d1857acd9fdefab59d348632844ca
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-21 17:56:40 -0600

    task 25

---
+++ csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Transpose(double[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        if (matrix == {{}})
+        {
+            double[,] emptyMatrix = {{}};
+            return emptyMatrix;
+        }
+
+        double[,] newMatrix = new double[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                newMatrix[j, i] = matrix[i, j];
+            }
+        }
+        return newMatrix;
+    }
+}
