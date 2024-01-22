92f529a135b80acd215353f7d13e0847b9a66a6e
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-21 19:52:46 -0600

    task 28 test

---
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double Determinant(double[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        if (rows != cols)
+        {
+            return -1;
+        }
+
+        if (rows == 2)
+        {
+            return (matrix[0,0] * matrix[1,1]) - (matrix[0,1] * matrix[1,0]);
+        }
+        else if (rows == 3)
+        {
+            return (matrix[0,0] * (matrix[1,1] * matrix[2,2] - matrix[1,2] * matrix[2,1])) 
+            - (matrix[0,1] * (matrix[1,0] * matrix[2,2] - matrix[1,2] * matrix[2,0])) 
+            + (matrix[0,2] * (matrix[1,0] * matrix[2,1] - matrix[1,1] * matrix[2,0]));
+        }
+    }
+}
