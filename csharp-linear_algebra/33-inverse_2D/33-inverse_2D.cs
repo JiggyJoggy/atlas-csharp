2723e922d7028617f6c825e98a0176c3df3a5a2f
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-21 22:00:25 -0600

    task 33 test

--- csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -7,19 +7,32 @@ class MatrixMath
         int rows = matrix.GetLength(0);
         int cols = matrix.GetLength(1);
 
-        if ((rows > 2 && cols > 2) && (rows != cols) && (rows <= 0 && cols <= 0))
+        double[,] badMatrix = {{-1}};
+
+        if ((rows != cols) || (rows <= 0 || cols <= 0) || (rows > 2 || cols > 2))
         {
-            return -1;
+            return badMatrix;
         }
 
         double[,] newMatrix = new double[rows, cols];
 
         double det = Math.Round((matrix[0,0] * matrix[1,1]) - (matrix[0,1] * matrix[1,0]), 2);
 
-        newMatrix[0,0] = Math.Round((matrix[1,1] / det)), 2;
-        newMatrix[0,1] = Math.Round((matrix[0,1] / det)), 2;
-        newMatrix[1,0] = Math.Round((matrix[1,0] / det), 2);
+        newMatrix[0,0] = Math.Round((matrix[1,1] / det), 2);
+        newMatrix[0,1] = Math.Round((-matrix[0,1] / det), 2);
+        newMatrix[1,0] = Math.Round((-matrix[1,0] / det), 2);
         newMatrix[1,1] = Math.Round((matrix[0,0] / det), 2);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (Double.IsInfinity(newMatrix[i,j]))
+                {
+                    return badMatrix;
+                }
+            }
+        }
 
         return newMatrix;
     }
