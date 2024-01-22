a276c75023477df7698dea507dc633b4d2157a44
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-21 21:40:01 -0600

    task 33

---
+++ csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Inverse2D(double[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        if ((rows > 2 && cols > 2) && (rows != cols) && (rows <= 0 && cols <= 0))
+        {
+            return -1;
+        }
+
+        double[,] newMatrix = new double[rows, cols];
+
+        double det = Math.Round((matrix[0,0] * matrix[1,1]) - (matrix[0,1] * matrix[1,0]), 2);
+
+        newMatrix[0,0] = Math.Round((matrix[1,1] / det)), 2;
+        newMatrix[0,1] = Math.Round((matrix[0,1] / det)), 2;
+        newMatrix[1,0] = Math.Round((matrix[1,0] / det), 2);
+        newMatrix[1,1] = Math.Round((matrix[0,0] / det), 2);
+
+        return newMatrix;
+    }
+}
