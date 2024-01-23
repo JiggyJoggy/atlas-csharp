0ad08555d88b0bcdffd8e96d2489b272eea3f02a
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-23 17:39:54 -0600

    task 0

---
+++ csharp-delegates_events/0-universal_health/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/// <summary>
+/// Setting up the Player
+/// </summary>
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    /// <summary>
+    /// Setting up player's name and maxHp
+    /// hp is set to maxHp at the start
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="maxHp"></param>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp < 0)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+        this.name = name;
+    }
+
+    /// <summary>
+    /// Method that prints out name of player, hp it has out of maxHp
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+}
