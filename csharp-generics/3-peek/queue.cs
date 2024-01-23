8e65d2a2ac7d7758c357f9d81c6cc01947eb496d
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-22 21:01:39 -0600

    task 3 test

---
+++ csharp-generics/3-peek/queue.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+class Queue<T>
+{
+    public Node head;
+    public Node tail;
+    public int count;
+
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+
+        if (tail == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        count++;
+    }
+
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+
+        T value = head.value;
+        head = head.next;
+        count--;
+
+        if (head == null)
+        {
+            tail = null;
+        }
+
+        return value;
+    }
+
+    public T Peek()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        return head.value;
+    }
+
+    public int Count()
+    {
+        return count;
+    }
+
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    public class Node
+    {
+        public T value { get; set; }
+        public Node next { get; set; }
+
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+
+    }
+}
