2311049295d5052ec27e37d21480c5346edf999f
JiggyJoggy <JaggerVanW@gmail.com>
2024-01-22 20:20:15 -0600

    task 2

---
+++ csharp-generics/2-dequeue/queue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Queue<T>
+{
+    public Node head;
+    public Node tail;
+    public int count;
+
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+
+        if (tail == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        count++;
+    }
+
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+
+        T value = head.value;
+        head = head.next;
+        count--;
+
+        return value;
+    }
+
+    public int Count()
+    {
+        return count;
+    }
+
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    public class Node
+    {
+        public T value { get; set; }
+        public Node next { get; set; }
+
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+
+    }
+}
